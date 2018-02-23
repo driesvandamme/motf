0a07956f9dfeb89ef95c40337c05070273a5c7b7
BolleBombastic <matthieuangel@hotmail.com>
2018-02-23 17:31:11 +0000

    Matthieu

--- webapp/controller/Carpooling.controller.js
+++ webapp/controller/Carpooling.controller.js
@@ -53,6 +53,116 @@ sap.ui.define(["sap/ui/core/mvc/Controller",
 			}
 			return oQuery;
 		},
+				doNavigate: function(sRouteName, oBindingContext, fnPromiseResolve, sViaRelation) {
+
+			var sPath = (oBindingContext) ? oBindingContext.getPath() : null;
+			var oModel = (oBindingContext) ? oBindingContext.getModel() : null;
+
+			var sEntityNameSet;
+			if (sPath !== null && sPath !== "") {
+				if (sPath.substring(0, 1) === "/") {
+					sPath = sPath.substring(1);
+				}
+				sEntityNameSet = sPath.split("(")[0];
+			}
+			var sNavigationPropertyName;
+			var sMasterContext = this.sMasterContext ? this.sMasterContext : sPath;
+
+			if (sEntityNameSet !== null) {
+				sNavigationPropertyName = sViaRelation || this.getOwnerComponent().getNavigationPropertyForNavigationWithContext(sEntityNameSet, sRouteName);
+			}
+			if (sNavigationPropertyName !== null && sNavigationPropertyName !== undefined) {
+				if (sNavigationPropertyName === "") {
+					this.oRouter.navTo(sRouteName, {
+						context: sPath,
+						masterContext: sMasterContext
+					}, false);
+				} else {
+					oModel.createBindingContext(sNavigationPropertyName, oBindingContext, null, function(bindingContext) {
+						if (bindingContext) {
+							sPath = bindingContext.getPath();
+							if (sPath.substring(0, 1) === "/") {
+								sPath = sPath.substring(1);
+							}
+						} else {
+							sPath = "undefined";
+						}
+
+						// If the navigation is a 1-n, sPath would be "undefined" as this is not supported in Build
+						if (sPath === "undefined") {
+							this.oRouter.navTo(sRouteName);
+						} else {
+							this.oRouter.navTo(sRouteName, {
+								context: sPath,
+								masterContext: sMasterContext
+							}, false);
+						}
+					}.bind(this));
+				}
+			} else {
+				this.oRouter.navTo(sRouteName);
+			}
+
+			if (typeof fnPromiseResolve === "function") {
+				fnPromiseResolve();
+			}
+		},
+				_onObjectListItemPress: function(oEvent) {
+
+			var oBindingContext = oEvent.getSource().getBindingContext();
+
+			return new Promise(function(fnResolve) {
+
+				this.doNavigate("CarpoolSubmit", oBindingContext, fnResolve, "");
+			}.bind(this)).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err.message);
+				}
+			});
+
+		},
+		_onObjectListItemPress1: function(oEvent) {
+
+			var oBindingContext = oEvent.getSource().getBindingContext();
+
+			return new Promise(function(fnResolve) {
+
+				this.doNavigate("CarpoolSubmit", oBindingContext, fnResolve, "");
+			}.bind(this)).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err.message);
+				}
+			});
+
+		},
+		_onObjectListItemPress2: function(oEvent) {
+
+			var oBindingContext = oEvent.getSource().getBindingContext();
+
+			return new Promise(function(fnResolve) {
+
+				this.doNavigate("CarpoolSubmit", oBindingContext, fnResolve, "");
+			}.bind(this)).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err.message);
+				}
+			});
+
+		},
+		_onObjectListItemPress3: function(oEvent) {
+
+			var oBindingContext = oEvent.getSource().getBindingContext();
+
+			return new Promise(function(fnResolve) {
+
+				this.doNavigate("CarpoolSubmit", oBindingContext, fnResolve, "");
+			}.bind(this)).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err.message);
+				}
+			});
+
+		},
 		onInit: function() {
 			this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 			this.oRouter.getTarget("Carpooling").attachDisplay(jQuery.proxy(this.handleRouteMatched, this));
